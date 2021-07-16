fb29a52ebe57334aa9628a603e48d49fc4fc26e8
gindle <87195821+gindle@users.noreply.github.com>
2021-07-16 16:28:34 +0200

    Update index.php
    add tax calc function

--- index.php
+++ index.php
@@ -20,3 +20,47 @@
     ?>
   </body>
 </html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP Store</title>
+  </head>
+  <body>
+    <?php
+	    $name = "PHP Store";
+			$credit = 1000; 
+
+	    echo "<h1>Welcome to ".$name."!</h1>";
+	    echo "<h2>You have $".$credit." in your wallet.</h2>";
+
+		  $products['Computer']=750;
+	    $products['Car']=15000;
+	    $products['iPhone']=1000;
+	    $products['Toaster']=75;
+
+	    foreach($products as $key => $value){
+		    echo "<p>The ".$key." costs ".$value."</p>";
+	    }
+
+	    echo "<h2>Items you can afford</h2>";
+
+	    foreach($products as $key => $value){
+		    if($value <= $credit ){
+		    	echo "<p>".$key."</p>"; 
+		    }
+			$amount=800;
+$taxRate=0.0825;
+$addedTax= $amount*$taxRate;  //amount = 800, tax = .0825
+echo $addedTax;
+
+	    }
+
+		function tax_calc(750m,0.223){
+			$calculate_tax = $amount*$tax;
+			$amount = round($amount+$calculate_tax,2);
+			return $amount;
+		}
+	  
+    ?>
+  </body>
+</html>
